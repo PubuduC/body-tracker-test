a37274da1840054550c8c48dd8d92311102aa00f
Pubudu Cooray <pubuduc.14@cse.mrt.ac.lk>
2017-07-23 15:45:03 +0530

    initial commit

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function getTestPage(Request $request){
+        return view('pages.test')->with("content", $request);
+    }
+}
