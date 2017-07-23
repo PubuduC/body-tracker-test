de3e7279c7e1574883141eea4b325d058b8394f6
Pubudu Cooray <pubuduc.14@cse.mrt.ac.lk>
2017-07-23 17:41:04 +0530

    database migrations added

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,8 +7,6 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
     public function getTestPage(Request $request){
-//        return view('pages.test')->with("content", $request);
-        return json_decode($request->getContent());
-
+        return $request;
     }
 }
