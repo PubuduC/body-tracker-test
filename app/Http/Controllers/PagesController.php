aef42f737530bbd4040cf80b9325f8f4dc379b7b
Pubudu Cooray <pubuduc.14@cse.mrt.ac.lk>
2017-07-23 16:49:21 +0530

    database configuration added

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -9,6 +9,5 @@ class PagesController extends Controller
     public function getTestPage(Request $request){
 //        return view('pages.test')->with("content", $request);
         return json_decode($request->getContent());
-//        return $request;
     }
 }
