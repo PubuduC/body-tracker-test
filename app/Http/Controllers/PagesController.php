1be3be50a2801ec01a0cd30522701bcb09a4d6aa
Pubudu Cooray <pubuduc.14@cse.mrt.ac.lk>
2017-07-23 17:53:55 +0530

    changed controller

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Patch;
 use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    public function getTestPage(Request $request){
-        return $request->id;
+    public function getTestPage(Request $request)
+    {
+        $patch = new Patch();
+        $patch->x = $request->x;
+        $patch->y = $request->y;
+        $patch->z = $request->z;
+        $patch->save();
+        return true;
     }
 }
