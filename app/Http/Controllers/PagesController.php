13438d823c08d494b296bbc7db5e2e844e0cce8c
Pubudu Cooray <pubuduc.14@cse.mrt.ac.lk>
2017-07-23 16:36:11 +0530

    changed controller

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -8,7 +8,7 @@ class PagesController extends Controller
 {
     public function getTestPage(Request $request){
 //        return view('pages.test')->with("content", $request);
-        return json_decode($request);
+        return json_decode($request->getContent());
 //        return $request;
     }
 }
